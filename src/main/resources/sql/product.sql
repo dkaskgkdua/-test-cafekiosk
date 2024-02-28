031b8f10af5a9c93541a7d7d1da1f439b93f3cd6
dkask <dkaskgkdua@gmail.com>
2024-02-29 01:52:12 +0900

    persistence layer 테스트 구성 작성

---
+++ src/main/resources/sql/product.sql
@@ -0,0 +1,6 @@
+insert into product(name, price, product_number, selling_status, type)
+values("아메리카노", 4000, "001", "SELLING", "HANDMADE");
+insert into product(name, price, product_number, selling_status, type)
+values("카페라떼", 4500, "002", "HOLD", "HANDMADE");
+insert into product(name, price, product_number, selling_status, type)
+values("크루아상", 3500, "003", "STOP_SELLING", "BAKERY");
