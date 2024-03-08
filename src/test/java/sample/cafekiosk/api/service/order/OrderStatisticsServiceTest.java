43c0e0fcbf2726b5f7ba5765a1407b192ac1d01e
dkask <dkaskgkdua@gmail.com>
2024-03-08 21:03:11 +0900

    Mockito로 Stubbing 하기
    - h2db로 변환
    - 아키텍처 계층별 DTO 분리

---
+++ src/test/java/sample/cafekiosk/api/service/order/OrderStatisticsServiceTest.java
@@ -0,0 +1,105 @@
+package sample.cafekiosk.api.service.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.client.mail.MailSendClient;
+import sample.cafekiosk.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.domain.history.mail.MailSendHistoryRepository;
+import sample.cafekiosk.domain.order.Order;
+import sample.cafekiosk.domain.order.OrderRepository;
+import sample.cafekiosk.domain.order.OrderStatus;
+import sample.cafekiosk.domain.orderproduct.OrderProductRepository;
+import sample.cafekiosk.domain.product.Product;
+import sample.cafekiosk.domain.product.ProductRepository;
+import sample.cafekiosk.domain.product.ProductType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static sample.cafekiosk.domain.product.ProductSellingStatus.SELLING;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("local")
+class OrderStatisticsServiceTest {
+    @Autowired
+    private OrderStatisticsService orderStatisticsService;
+
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @MockBean
+    private MailSendClient mailSendClient;
+
+    @DisplayName("결제완료 주문들을 조회하여 매출 통계 메일을 전송한다.")
+    @Test
+    void sendOrderStatisticsMail() {
+        LocalDateTime now = LocalDateTime.of(2023, 3, 5, 0, 0);
+        // given
+        Product product1 =createProduct(ProductType.HANDMADE, "001", 1000);
+        Product product2 =createProduct(ProductType.HANDMADE, "002", 2000);
+        Product product3 =createProduct(ProductType.HANDMADE, "003", 3000);
+        List<Product> products = List.of(product1, product2, product3);
+        productRepository.saveAll(products);
+
+        Order order1 = createPaymentCompletedOrder(LocalDateTime.of(2023, 3, 4, 23, 59, 59), products);
+        Order order2 = createPaymentCompletedOrder(now, products);
+        Order order3 = createPaymentCompletedOrder(LocalDateTime.of(2023, 3, 5, 23, 59, 59), products);
+        Order order4 = createPaymentCompletedOrder(LocalDateTime.of(2023, 3, 6, 0, 0), products);
+
+
+        // MailSendClient의 sendEmail에 String값 파라미터 아무거나 오고, 결과는 반드시 true로 해달라는 가상의 빈
+        // stubbing
+        Mockito.when(mailSendClient.sendEmail(any(String.class), any(String.class),any(String.class),any(String.class)))
+                .thenReturn(true);
+
+        // when
+        boolean result = orderStatisticsService.sendOrderStatisticsMail(LocalDate.of(2023, 3, 5), "test@test.com");
+
+        // then
+        assertThat(result).isTrue();
+
+        List<MailSendHistory> histories = mailSendHistoryRepository.findAll();
+        assertThat(histories).hasSize(1)
+                .extracting("content")
+                .contains("총 매출 합계는 12000원입니다.");
+    }
+
+    private Order createPaymentCompletedOrder(LocalDateTime now, List<Product> products) {
+        Order order = Order.builder()
+                .products(products)
+                .orderStatus(OrderStatus.PAYMENT_COMPLETED)
+                .registeredDateTime(now)
+                .build();
+        return orderRepository.save(order);
+    }
+
+    private Product createProduct(ProductType type, String productNumber, int price) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .type(type)
+                .sellingStatus(SELLING)
+                .name("아메리카노")
+                .price(price)
+                .name("상품")
+                .build();
+    }
+}
